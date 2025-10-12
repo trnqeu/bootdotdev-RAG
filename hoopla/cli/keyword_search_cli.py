d457d974a5484eaf4be4030dfa33196815a03a5c
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-10-12 06:41:58 +0200

    finished search with stopwords removal and revised the matching logic to match substrings

--- hoopla/cli/keyword_search_cli.py
+++ hoopla/cli/keyword_search_cli.py
@@ -2,11 +2,33 @@
 
 import argparse
 import json
+import string
 
 with open('./data/movies.json', 'r') as f:
     movies_dict = json.load(f)
 
 movies_list = movies_dict['movies']
+
+PUNCTUATION_REMOVER = str.maketrans("", "", string.punctuation)
+
+with open('data/stopwords.txt', 'r') as f:
+    text = f.read()
+
+STOPWORDS_SET = set(text.splitlines())
+
+def remove_punctuation(text: str) -> str:
+    # Remove punctuation from a string
+    return text.translate(PUNCTUATION_REMOVER)
+
+def tokenizer(text: str, stopwords: set[str]) -> set[str]:
+    # word tokenizer
+    processed_text = remove_punctuation(text).lower()
+    
+    # split by whitespace to get tokens
+    tokens = {token for token in processed_text.split() if token}
+
+    # use set difference to remove stopwords
+    return tokens - stopwords
 
 
 
@@ -24,12 +46,42 @@ def main() -> None:
     match args.command:
         case "search":
             query = args.query
+            # processed_query = remove_punctuation(query).lower()
+            query_tokens = tokenizer(query, STOPWORDS_SET)
+            
+            
             # print the search query here
             print(f"Searching for: {query}")
 
-            for movie in movies_list:                
-                if query.lower() in movie['title'].lower():
+            if not query_tokens:
+                print("Invalid search")
+                return
+
+            for movie in movies_list: 
+                # remove punctuation from title
+                # processed_title = remove_punctuation(movie['title']).lower()              
+                title_tokens = tokenizer(movie['title'], STOPWORDS_SET)
+                
+                # set.intersection() returns a new set containing elements common to both sets.
+                # If the resulting set is non-empty, there is at least one matching token.
+                # matching_tokens = [query_tokens.intersection(title_tokens) ]
+                # new matching logic to match substrings, like 'shot' in 'killshot'
+                is_match = False
+
+                for q_token in query_tokens:
+                    for t_token in title_tokens:
+                        if q_token in t_token:
+                            is_match = True
+                            break
+                    if is_match:
+                        break
+
+                if is_match:
                     results.append(movie)
+
+
+
+               
               
 
             results.sort(key= lambda movie: movie['id'], reverse=False)
