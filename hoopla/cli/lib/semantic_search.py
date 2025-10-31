b8f80bac68721193880cad4673307100c837b76d
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-10-31 05:27:43 +0100

    finished semantic search lib

--- hoopla/cli/lib/semantic_search.py
+++ hoopla/cli/lib/semantic_search.py
@@ -1,13 +1,42 @@
 from sentence_transformers import SentenceTransformer
-
+import numpy as np
 
 
 class SemanticSearch:
     def __init__(self, model_name="all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+
+
+    def generate_embedding(self, text):
+        if not text or text.isspace():
+            raise ValueError('Invalid values')
+        else:
+            return self.model.encode([text])[0]
+        
+    def build_embeddings(self, documents):
+        self.documents = documents
+        for doc in self.documents.items():
+            self.document_map[doc.id] = doc
+        docs_list = [f"{doc['title']}: {doc['description']}" for doc in self.documents.items()]
 
     
 def verify_model():
     search = SemanticSearch()
     print(f"Model loaded: {search.model}")
     print(f"Max sequence length: {search.model.max_seq_length}")
+
+def embed_text(text):
+    # creates an instance of the class
+    searcher = SemanticSearch()
+    # get the embedding
+    embedding = searcher.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
+# text = 'pippo beveva la piooggia'
+
+# print(searcher.generate_embedding(text))
--- hoopla/cli/semantic_search_cli.py
+++ hoopla/cli/semantic_search_cli.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest='command', help='available commands')
     verify_parser = subparsers.add_parser("verify", help="Verify that the embedding model is loaded")
+    text_embed_parser = subparsers.add_parser("embed_text", help='text to be embedded')
+    text_embed_parser.add_argument("text", type=str, help='text to be embedded')
 
     args = parser.parse_args()
 
     match args.command:
         case 'verify':
             verify_model()
+
+        case 'embed_text':
+            embed_text(args.text)
 
         case _:
             parser.print_help()
