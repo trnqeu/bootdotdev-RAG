63a2324f8c365ab790d62dc615f6e844b6ca3f66
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-12-24 06:48:58 +0100

    finished the rag function

--- hoopla/cli/lib/augmented_generation.py
+++ hoopla/cli/lib/augmented_generation.py
@@ -21,4 +21,20 @@ print(f"Using key {api_key[:6]}...")
 client = genai.Client(api_key=api_key)
 
 def rag_command(query):
-    pass
+    movies = load_movies()
+    search = HybridSearch(movies)
+    docs = search.rrf_search(query, 60, 5)
+    prompt = f"""Answer the question or provide information based on the provided documents. This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+
+    Query: {query}
+
+    Documents:
+    {docs}
+
+    Provide a comprehensive answer that addresses the query:"""
+
+    response = client.models.generate_content(
+            model='gemini-2.0-flash-001',
+            contents = prompt)
+    
+    return docs, response
